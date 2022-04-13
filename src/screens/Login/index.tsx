e10af84151d8c0f3eaa31745b1372b3065af4478
lazarodu <lazarodu@gmail.com>
2022-04-13 07:57:13 -0300

    Aula do dia 13/04/2022

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,5 +1,11 @@
 import React from "react";
-import { View, Text, KeyboardAvoidingView, TextInput } from "react-native";
+import {
+  View,
+  Text,
+  KeyboardAvoidingView,
+  TextInput,
+  ImageBackground,
+} from "react-native";
 import { MaterialIcons, Entypo } from "@expo/vector-icons";
 import Button from "../../components/Button";
 import styles from "./styles";
@@ -11,29 +17,34 @@ export default function Login() {
 
   return (
     <View style={styles.container}>
-      <KeyboardAvoidingView>
-        <Text style={styles.title}>Login</Text>
-        <View style={styles.formRow}>
-          <MaterialIcons name="email" style={styles.icon} />
-          <TextInput
-            style={styles.input}
-            placeholder="Email"
-            keyboardType="email-address"
-            autoCapitalize="none"
-          />
-        </View>
-        <View style={styles.formRow}>
-          <Entypo name="key" style={styles.icon} />
-          <TextInput
-            style={styles.input}
-            placeholder="Senha"
-            secureTextEntry={true}
-            autoCapitalize="none"
-          />
-        </View>
-        <Button title="Login" type="green" onPress={handleSignIn} />
-        <Button title="Cadastre-se" type="purple" onPress={handleSignIn} />
-      </KeyboardAvoidingView>
+      <ImageBackground
+        source={require("../../assets/fundo.png")}
+        style={styles.container}
+      >
+        <KeyboardAvoidingView>
+          <Text style={styles.title}>Login</Text>
+          <View style={styles.formRow}>
+            <MaterialIcons name="email" style={styles.icon} />
+            <TextInput
+              style={styles.input}
+              placeholder="Email"
+              keyboardType="email-address"
+              autoCapitalize="none"
+            />
+          </View>
+          <View style={styles.formRow}>
+            <Entypo name="key" style={styles.icon} />
+            <TextInput
+              style={styles.input}
+              placeholder="Senha"
+              secureTextEntry={true}
+              autoCapitalize="none"
+            />
+          </View>
+          <Button title="Login" type="green" onPress={handleSignIn} />
+          <Button title="Cadastre-se" type="purple" onPress={handleSignIn} />
+        </KeyboardAvoidingView>
+      </ImageBackground>
     </View>
   );
 }
