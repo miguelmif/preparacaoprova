36e45d1c992bb3a92402cd45d630cfa435c4efbd
lazarodu <lazarodu@gmail.com>
2022-04-20 10:14:58 -0300

    Aula do dia 20/04/2022

--- src/screens/Perfil/index.tsx
+++ src/screens/Perfil/index.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { View, Text, ImageBackground, Image } from "react-native";
+import styles from "./styles";
 
 export default function Perfil() {
   return (
-    <View>
-      <ImageBackground source={require("../../assets/fundo.png")}>
+    <View style={styles.container}>
+      <ImageBackground
+        source={require("../../assets/fundo.png")}
+        style={styles.container}
+      >
         <Image source={require("../../assets/lazaro.png")} />
         <Text>Lázaro Eduardo da Silva</Text>
       </ImageBackground>
