dc019425d31851276e353f48a2ad16fc80fa58b8
lazarodu <lazarodu@gmail.com>
2022-04-27 10:11:45 -0300

    Aula do dia 27/04/2022

---
+++ src/services/data.ts
@@ -0,0 +1,33 @@
+export default [
+  {
+    id: 1,
+    nome: "Lázaro",
+    data: "27/04/2022 10:08:00",
+    titulo: "Blusa",
+    mensagem: "Tenho uma blusa para doar, alguém interessa?",
+    topico: [{
+      id: 1,
+      item: "Reutilizar"
+    }, {
+      id: 2,
+      item: "Reciclar"
+    }]
+  },
+  {
+    id: 2,
+    nome: "Luan",
+    data: "28/04/2022 10:08:00",
+    titulo: "Calça",
+    mensagem: "Tenho uma calça para doar, alguém interessa?",
+    topico: [{
+      id: 1,
+      item: "Reutilizar"
+    }, {
+      id: 2,
+      item: "Reciclar"
+    }, {
+      id: 3,
+      item: "Doar"
+    }]
+  },
+]
