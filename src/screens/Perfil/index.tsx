774076d93bbbc47934580f3fd7dd3c4fd8a893cb
lazarodu <lazarodu@gmail.com>
2022-04-27 10:06:51 -0300

    Aula do dia 27/04/2022

--- src/screens/Perfil/index.tsx
+++ src/screens/Perfil/index.tsx
@@ -1,6 +1,9 @@
 import React from "react";
-import { View, Text, ImageBackground, Image } from "react-native";
+import { View, Text, ImageBackground, Image, TextInput } from "react-native";
+import CardSocial from "../../components/CardSocial";
+import { FontAwesome5 } from "@expo/vector-icons";
 import styles from "./styles";
+import Button from "../../components/Button";
 
 export default function Perfil() {
   return (
@@ -11,6 +14,35 @@ export default function Perfil() {
       >
         <Image source={require("../../assets/lazaro.png")} />
         <Text>Lázaro Eduardo da Silva</Text>
+        <CardSocial>
+          <>
+            <FontAwesome5 name="facebook" />
+            <TextInput placeholder="https://facebook.com" />
+          </>
+        </CardSocial>
+        <CardSocial>
+          <>
+            <FontAwesome5 name="instagram" />
+            <TextInput placeholder="https://instagram.com" />
+          </>
+        </CardSocial>
+        <CardSocial>
+          <>
+            <FontAwesome5 name="linkedin" />
+            <TextInput placeholder="https://linkedin.com" />
+          </>
+        </CardSocial>
+        <Button
+          title="Salvar"
+          type="third"
+          onPress={() => console.log("Salvar")}
+        />
+        <Button
+          title="Alterar Senha"
+          type="third"
+          onPress={() => console.log("Alterar Senha")}
+        />
+        <Button title="Sair" type="third" onPress={() => console.log("Sair")} />
       </ImageBackground>
     </View>
   );
