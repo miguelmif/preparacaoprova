b0ec18870be4695e276a954e8074fc0e0b4c391f
lazarodu <lazarodu@gmail.com>
2022-04-27 10:39:44 -0300

    Aula do dia 27/04/2022

--- src/screens/Chat/index.tsx
+++ src/screens/Chat/index.tsx
@@ -9,14 +9,18 @@ import {
 import { FontAwesome5 } from "@expo/vector-icons";
 import data from "../../services/data";
 import Card from "../../components/Card";
+import styles from "./styles";
 
 export default function Chat() {
   const renderItem = ({ item }: any) => <Card data={item} />;
   return (
-    <ImageBackground source={require("../../assets/fundo.png")}>
-      <SafeAreaView>
-        <View>
-          <FontAwesome5 name="search" size={24} color="black" />
+    <ImageBackground
+      source={require("../../assets/fundo.png")}
+      style={styles.container}
+    >
+      <SafeAreaView style={styles.container}>
+        <View style={styles.rowSearch}>
+          <FontAwesome5 name="search" style={styles.icon} />
           <TextInput placeholder="Pesquisar chat" />
         </View>
         <FlatList
